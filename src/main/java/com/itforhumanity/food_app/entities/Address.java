775ae1f85bbc594bc83e13e5bcdf982c200d1548
hrdlifil <filip.hrdlicka@gmail.com>
2018-02-03 22:59:03 +0100

    data entity

---
+++ src/main/java/com/itforhumanity/food_app/entities/Address.java
@@ -0,0 +1,63 @@
+package com.itforhumanity.food_app.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Address implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+
+    private int houseNumber;
+
+    private String town;
+
+    private int postalCode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+}
--- src/main/java/com/itforhumanity/food_app/model/Address.java
+++
@@ -1,4 +0,0 @@
-package com.itforhumanity.food_app.model;
-
-public class Address {
-}
